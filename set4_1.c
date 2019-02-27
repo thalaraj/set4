75c2e78d6ce8df7d9fc0efc4a00b8efd3368d5ce
thalaraj <47813910+thalaraj@users.noreply.github.com>
2019-02-26 16:20:58 -0800

    Create set4_1.c

---
+++ set4_1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+  char a[100];
+  int b=0,i;
+  for(i=0;a[i]<=20;i++)
+  {
+    scanf("%c",&a[i]);
+    if((a[i]>='a')&&(a[i]<='z')&&(a[i]!=' '))
+    {
+    b=b+1;
+    }
+  }
+  printf("%d",b);
+return 0;
+}
